e2e3ede4d5a9f87e8ad2192077b669e07597ad08
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2022-01-17 16:58:05 +0100

    Edit cors in api

--- api/server.js
+++ api/server.js
@@ -20,12 +20,7 @@ mongoose
   .then(() => console.log("db connection successfull"))
   .catch((err) => console.log(err));
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
